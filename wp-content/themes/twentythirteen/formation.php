cf95101605a8f8267f273b73945e79e246a640af
Nuwandi <Nuwandi@MC-G-LPCc120-18.local>
2014-03-11 15:33:29 +0100

    ajout des droits admin sur le front page formation

--- wp-content/themes/twentythirteen/formation.php
+++ wp-content/themes/twentythirteen/formation.php
@@ -6,6 +6,10 @@
  */
 
 get_header();  
+$query = new WP_Query(array('post_type' => 'formation')); 
+
+global $current_user;
+get_currentuserinfo();
 ?>
  
 <div id="primary" class="content-area">
@@ -13,11 +17,11 @@ get_header();
     <article style="width:100%;margin:20px auto;max-width:604px;">
       <h1>List of courses</h1>
 
-      <?php $query = new WP_Query(array('post_type' => 'formation')); ?>
       <table>
         <th>Title</th>
         <th>Diploma</th>
         <th>Conditions of acceptance</th>
+        <?php if ($current_user->user_login == 'nuwandi') {echo '<th></th>';} ?>
         <?php while ( $query->have_posts() ) : $query->the_post(); ?>
           <tr>
               <td>
@@ -29,6 +33,7 @@ get_header();
               <td>
                 <?php the_field('condition'); ?>
               </td>
+              <?php if ($current_user->user_login == 'nuwandi') {echo '<td><a href="/intraUniversity/wp-admin/edit.php?post_type=formation">EDIT</a></td>';} ?>
           </tr>
         <?php endwhile; // end of the loop. ?>
       </table>
